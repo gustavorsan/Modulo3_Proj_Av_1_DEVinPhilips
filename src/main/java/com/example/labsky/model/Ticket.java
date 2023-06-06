22b23b40059b29c227b25f7f5d214b0ea7f5db7b
gustavorsan <gustavorsantos42@gmail.com>
2023-06-06 20:34:39 -0300

    Adcionado anotação de data no ticket e criação do controller

--- src/main/java/com/example/labsky/model/Ticket.java
+++ src/main/java/com/example/labsky/model/Ticket.java
@@ -1,10 +1,12 @@
 package com.example.labsky.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
+@Data
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
