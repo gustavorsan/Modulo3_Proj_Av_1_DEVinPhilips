daadbe2740e98d57a4b1b364fb27156c2d233c72
gustavorsan <gustavorsantos42@gmail.com>
2023-06-06 20:10:42 -0300

    first commit

---
+++ src/test/java/com/example/labsky/LabSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
